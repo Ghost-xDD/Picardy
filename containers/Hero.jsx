0b7298495d1081144f2e67bfd586d8108b9074c6
GhostxD <ademolajohn844@gmail.com>
2023-01-23 22:13:28 +0300

    pushed

--- containers/Hero.jsx
+++ containers/Hero.jsx
@@ -63,77 +63,6 @@ const firstName = {
   },
 };
 
-const lastName = {
-  initial: {
-    y: -20,
-  },
-  animate: {
-    y: 0,
-    transition: {
-      delayChildren: 0.4,
-      staggerChildren: 0.04,
-      staggerDirection: 1,
-    },
-  },
-};
-
-const letter = {
-  initial: {
-    y: 400,
-  },
-  animate: {
-    y: 0,
-    transition: { duration: 1, ...transition },
-  },
-};
-
-const btnGroup = {
-  initial: {
-    y: -60,
-    opacity: 0,
-    transition: { duration: 0.6, ease: easeing },
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    animation: {
-      duration: 0.6,
-      ease: easeing,
-    },
-  },
-};
-const star = {
-  initial: {
-    y: 60,
-    opacity: 0,
-    transition: { duration: 0.8, ease: easeing },
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    animation: {
-      duration: 0.6,
-      ease: easeing,
-    },
-  },
-};
-
-const header = {
-  initial: {
-    y: -60,
-    opacity: 0,
-    transition: { duration: 0.05, ease: easeing },
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    animation: {
-      duration: 0.6,
-      ease: easeing,
-    },
-  },
-};
-
 const Hero = () => {
   return (
     <>
@@ -155,7 +84,7 @@ const Hero = () => {
           <Button props="Join Our Waitlist" />
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <button
-            className={`${euclid.variable} font-serif text-[#F9CC16] uppercase font-bold text-[12px] md:text-md`}
+            className={`${lemonMilk.variable} font-sans text-[#F9CC16] uppercase font-bold text-[12px]  md:text-[16px]`}
           >
             Read Docs
           </button>
